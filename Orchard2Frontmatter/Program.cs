6ceaabf2be9e5210d20e2463e298a8e0ca7da93f
Hazzamanic <breakitup@hotmail.co.uk>
2020-02-10 22:06:53 +0000

    initial commit

---
+++ Orchard2Frontmatter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orchard2Frontmatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
