3e00462b5fdf147b33863cad5bcd5799b911d10f
Hazzamanic <breakitup@hotmail.co.uk>
2020-02-12 09:03:13 +0000

    load file from current executing location

--- Orchard2Frontmatter/Program.cs
+++ Orchard2Frontmatter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -8,8 +10,34 @@ namespace Orchard2Frontmatter
     {
         static void Main(string[] args)
         {
-            var doc = XDocument.Load(@"C:\Users\break\Source\Repos\Orchard2Frontmatter\Orchard2Frontmatter\data\export.xml");
-            Console.WriteLine(doc.Descendants("Data").Descendants("BlogPost").FirstOrDefault().Element("TitlePart").Attribute("Title").Value);
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = System.IO.Path.GetDirectoryName(path);
+            var doc = XDocument.Load(Path.Combine(path, "data", "export"));
+            var posts = new List<Post>();
+            
+
+            foreach(var blogPost in doc.Descendants("Data").Descendants("BlogPost"))
+            {
+                if (blogPost.Attribute("Status").Value != "Published")
+                    continue;
+
+
+
+                posts.Add(new Post
+                {
+                    Title = blogPost.Element("TitlePart").Attribute("Title").Value,
+
+                });
+            }
+        }
+
+        class Post
+        {
+            public string Title { get; set; }
+            public DateTime Created { get; set; }
+            public string Body { get; set; }
+            public string TagsRaw { get; set; }
+            public string[] Tags => TagsRaw?.Split(',') ?? new string[0];
         }
     }
 }
